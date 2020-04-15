530c3871aded5d2aa5977ee7a8802d0323109070
Nedeljko Vidovic <vidovicnedeljko32@gmail.com>
2020-04-15 17:41:43 +0200

    Aggiunto test per hello controller 2

---
+++ src/test/java/it/corso/esempio/controller/HelloControllerTest2.java
@@ -0,0 +1,32 @@
+package it.corso.esempio.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import it.corso.esempio.pojo.HelloResponse;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HelloControllerTest2 {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void testStatusOk() {
+		HelloResponse actual = this.restTemplate.getForObject("/hello2/status", HelloResponse.class);
+		HelloResponse expected = buildExpectedOkResponse();
+		assertThat(actual).isEqualTo(expected);
+	}
+
+	private HelloResponse buildExpectedOkResponse() {
+		HelloResponse res = new HelloResponse();
+		res.setStatus("OK");
+		return res;
+	}
+
+}
